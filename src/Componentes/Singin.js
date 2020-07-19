ca9adb8957aac10322e5f3485776b8d537b65d50
manlio <manliot@uninorte.edu.co>
2020-07-18 22:59:32 -0500

    acomodando estilos del registrar componente

--- src/Componentes/Singin.js
+++ src/Componentes/Singin.js
@@ -13,18 +13,13 @@ export default function Singin({ route, navigation }) {
   const [Age, setAge] = useState('')
   const [UserName, setUserName] = useState('')
   const [Password, setPassword] = useState('')
-  //refs
   const name_ref = useRef(null)
   const lastname_ref = useRef(null)
   const edad_ref = useRef(null)
   const username_ref = useRef(null)
   const password_ref = useRef(null)
-  //globalState
-
-  //Dispach
   const db = useSelector(state => state.db)
   const db2 = useSelector(state => state.db2)
-  //methods
   const vaciarFormulario = () => {// vacia el formulario
     name_ref.value = ''
     lastname_ref.value = ''
@@ -32,79 +27,79 @@ export default function Singin({ route, navigation }) {
     username_ref.value = ''
     password_ref.value = ''
   }
- /*  const onPressRegisterButton = () => {//realiza un query a la base de datos para añadir un nuevo usuario.
-    if (Name == '' || LastName == '' || Age == '' || UserName == '' || Password == '') {
-      Toast.show(
-        {
-          text: 'Por favor, Complete toda la informacion',
-          buttonText: 'Ok', position: 'bottom',
-          type: "warning", onClose: () => { console.log('') },
-          duration: 30000,
-        }
-      )
-    } else {
-      if (!isNaN(Age)) {
-        vaciarFormulario()
-        db2.collection("Usuario").add({
-          Usuario: UserName,
-          Contraseña: Password,
-          Nombre: Name,
-          Apellido: LastName,
-          Edad: parseInt(Age)
-        })
-          .then((mes) => {
-            Toast.show(
-              {
-                text: 'Fuiste registrado satifactoriamente en cloud',
-                buttonText: 'Ok', position: 'bottom',
-                type: "success", onClose: () => { navigation.goBack() },
-                duration: 30000,
-              }
-            )
-            console.log(mes)
-          })
-          .catch((err) => console.error(new Error(err)))
-        /*         db.transaction(tx => {
-                  tx.executeSql(
-                    'INSERT INTO Usuario (Usuario, Contraseña, Nombre, Apellido, Edad) VALUES (?,?,?,?,?)',
-                    [UserName, Password, Name, LastName, Age],
-                    (tx, res) => {
-                      if (res.rowsAffected > 0) {
-                        Toast.show(
-                          {
-                            text: 'Fuiste registrado satifactoriamente',
-                            buttonText: 'Ok', position: 'bottom',
-                            type: "success", onClose: () => { navigation.goBack() },
-                            duration: 30000,
-                          }
-                        )
-                      } else {
-                        Toast.show(
-                          {
-                            text: 'Error al registrarse, Vuela a intentar',
-                            buttonText: 'Okay', position: 'bottom',
-                            type: "danger", onClose: () => { console.log('') }
-                          }
-                        )
-                        alert('fallo al registrarse');
-                      }
-                    }
-                  )
-                }, () => {
-                  username_ref.value = ''
-                  Toast.show(
-                    {
-                      text: 'Este nombre de usuario ya fue escogido :c',
-                      buttonText: 'Okay', position: 'bottom',
-                      type: "danger", onClose: () => { console.log('pero que ha pasao') }
-                    }
-                  )
-                }); 
-      } else {
-        Alert.alert('', 'La edad tiene que ser un valor numerico')
-      }
-    }
-  } */
+  /*  const onPressRegisterButton = () => {//realiza un query a la base de datos para añadir un nuevo usuario.
+     if (Name == '' || LastName == '' || Age == '' || UserName == '' || Password == '') {
+       Toast.show(
+         {
+           text: 'Por favor, Complete toda la informacion',
+           buttonText: 'Ok', position: 'bottom',
+           type: "warning", onClose: () => { console.log('') },
+           duration: 30000,
+         }
+       )
+     } else {
+       if (!isNaN(Age)) {
+         vaciarFormulario()
+         db2.collection("Usuario").add({
+           Usuario: UserName,
+           Contraseña: Password,
+           Nombre: Name,
+           Apellido: LastName,
+           Edad: parseInt(Age)
+         })
+           .then((mes) => {
+             Toast.show(
+               {
+                 text: 'Fuiste registrado satifactoriamente en cloud',
+                 buttonText: 'Ok', position: 'bottom',
+                 type: "success", onClose: () => { navigation.goBack() },
+                 duration: 30000,
+               }
+             )
+             console.log(mes)
+           })
+           .catch((err) => console.error(new Error(err)))
+         /*         db.transaction(tx => {
+                   tx.executeSql(
+                     'INSERT INTO Usuario (Usuario, Contraseña, Nombre, Apellido, Edad) VALUES (?,?,?,?,?)',
+                     [UserName, Password, Name, LastName, Age],
+                     (tx, res) => {
+                       if (res.rowsAffected > 0) {
+                         Toast.show(
+                           {
+                             text: 'Fuiste registrado satifactoriamente',
+                             buttonText: 'Ok', position: 'bottom',
+                             type: "success", onClose: () => { navigation.goBack() },
+                             duration: 30000,
+                           }
+                         )
+                       } else {
+                         Toast.show(
+                           {
+                             text: 'Error al registrarse, Vuela a intentar',
+                             buttonText: 'Okay', position: 'bottom',
+                             type: "danger", onClose: () => { console.log('') }
+                           }
+                         )
+                         alert('fallo al registrarse');
+                       }
+                     }
+                   )
+                 }, () => {
+                   username_ref.value = ''
+                   Toast.show(
+                     {
+                       text: 'Este nombre de usuario ya fue escogido :c',
+                       buttonText: 'Okay', position: 'bottom',
+                       type: "danger", onClose: () => { console.log('pero que ha pasao') }
+                     }
+                   )
+                 }); 
+       } else {
+         Alert.alert('', 'La edad tiene que ser un valor numerico')
+       }
+     }
+   } */
   const onPressRegisterButton = () => {//realiza un query a la base de datos para añadir un nuevo usuario.
     if (Name == '' || LastName == '' || Age == '' || UserName == '' || Password == '') {
       Toast.show(
@@ -161,12 +156,11 @@ export default function Singin({ route, navigation }) {
   }
   return (
     <View style={stylesRegister.container} >
-      <Image style={[stylesLogin.fondo, { position: 'absolute', top: 0, width: '100%' }]} imageStyle={{ resizeMode: 'center', height: '25%', width: '100%' }} source={require('../../assets/images/FONDO.png')} />
-      <ScrollView style={[{ width: '100%' }]}>
+      <ScrollView>
         <Container style={{ backgroundColor: 'transparent', justifyContent: "center" }}>
           <View style={[stylesLogin.box, { marginTop: 0 }]}>
-            <Image style={{ height: 80, width: 80, position: 'relative', top: -70, borderRadius: 50 }} source={require('../../assets/images/userReg.png')} />
-            <TouchableHighlight activeOpacity={1} underlayColor='#ecfcff' style={{ width: 40, alignItems: 'center', position: 'relative', top: -108, left: '44%' }} onPress={() => navigation.goBack()}>
+            <Image style={stylesLogin.img_logoUser} source={require('../../assets/images/userReg.png')} />
+            <TouchableHighlight activeOpacity={1} underlayColor='#ecfcff' style={stylesRegister.goBack} onPress={() => navigation.goBack()}>
               <Feather name='x-circle' size={25} />
             </TouchableHighlight>
             <Text style={stylesRegister.subTitle}> Por favor, completa todos los campos. </Text>
--- src/Styles/RegisterStyles.js
+++ src/Styles/RegisterStyles.js
@@ -4,6 +4,7 @@ const width = (Dimensions.get('window').width)
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#65d359',
     },
     subTitle: {
         fontSize: 15,
@@ -11,5 +12,11 @@ const styles = StyleSheet.create({
         top: -75,
         color: 'gray',
     },
+    goBack: {
+        width: 40,
+        alignItems: 'center',
+        top: -108,
+        left: '44%'
+    }
 })
 module.exports = styles;
